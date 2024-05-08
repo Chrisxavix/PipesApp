c2ae13b0adcf48e6527936d3816235ba1b380bde
Chrisxavix <chrischillogallid64@outlook.com>
2024-05-08 00:05:20 -0500

    install and configuration primeng

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -3,11 +3,19 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputIconModule } from 'primeng/inputicon';
+import { IconFieldModule } from 'primeng/iconfield';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    BrowserAnimationsModule,
+    InputIconModule,
+    IconFieldModule,
+    ButtonModule
   ],
   bootstrap: [AppComponent],
 })
